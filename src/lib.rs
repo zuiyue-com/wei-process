26433181cc9ac740d4013254485086ca53b5840c
HuangWei <market@zuiyue.com>
2023-08-15 10:38:12 +0800

    0.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-pub fn run(cmd: &str) -> String {
+pub fn run(cmd: &str, param: &str) -> String {
     let output = std::process::Command::new("powershell")
-    .args(&["/C", "start", "-wait", &format!("\"{}\"", cmd)]).output().unwrap();
+    .args(&["/C", "start", "-wait", &format!("\"{}\"", cmd), param]).output().unwrap();
 
     let (res, _, _) = encoding_rs::UTF_16LE.decode(&output.stdout);
     res.to_string()
