ba84695be0a58f53983a3149ca9fad927130266b
HuangWei <market@zuiyue.com>
2023-08-15 15:27:11 +0800

    0.1.3

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
+use std::os::windows::process::CommandExt;
+
 pub fn run(cmd: &str, param: &str) -> Result<String, Box<dyn std::error::Error>> {
     let output = std::process::Command::new("powershell")
-    .args(&["/C", "start", "-wait", &format!("\"{}\"", cmd), param]).output()?;
+    .args(&["/C", "start", "-wait", &format!("\"{}\"", cmd), param])
+    .creation_flags(winapi::um::winbase::CREATE_NO_WINDOW)
+    .output()?;
 
     let (res, _, _) = encoding_rs::UTF_16LE.decode(&output.stdout);
     Ok(res.to_string())
