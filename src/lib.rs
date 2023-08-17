bc83262c70ae42d38a34f01a517cc45e65dd4740
HuangWei <market@zuiyue.com>
2023-08-17 10:45:42 +0800

    0.1.6

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,16 @@
 use std::os::windows::process::CommandExt;
 
 pub fn run(cmd: &str, param: Vec<String>) -> Result<String, Box<dyn std::error::Error>> {
+    let path = std::path::Path::new("./bin/").join(cmd).join(".exe");
+    if path.exists() {
+        return command(&path.display().to_string(), param);
+    }
+
+    let path = wei_env::read(&wei_env::dir_bin(),cmd)?;
+    command(path.as_str(), param)
+}
+
+pub fn command(cmd: &str, param: Vec<String>) -> Result<String, Box<dyn std::error::Error>> {
     //let output = std::process::Command::new("powershell")
     // .args(&["/C", "start", "-wait", &format!("\"{}\"", cmd), param])
     let output = std::process::Command::new(cmd)
@@ -15,4 +25,33 @@ pub fn run(cmd: &str, param: Vec<String>) -> Result<String, Box<dyn std::error::
         Ok(v) => Ok(v.to_string()),
         Err(e) => Err(Box::new(e))
     }    
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_run_echo() -> Result<(), Box<dyn std::error::Error>> {
+        // 使用 cmd 和 echo 命令来测试
+        let output = run("cmd", vec!["/C".to_string(), "echo Hello, world!".to_string()])?;
+        // 检查输出是否为期望的值
+        assert_eq!(output.trim(), "Hello, world!"); 
+        Ok(())
+    }
+
+    #[test]
+    fn test_run_wei_sd_exe() -> Result<(), Box<dyn std::error::Error>> {
+        let cmd_path = "C:\\Users\\Wei\\Desktop\\work\\wei-sd\\target\\debug\\wei-sd.exe";
+
+        let output = run(cmd_path, vec![])?;
+
+        // 打印输出的数据
+        println!("Output from {}: {}", cmd_path, output.trim());
+
+        // 这里你可以检查输出是否为期望的值。但在此例子中，我们只检查输出是否为空。
+        assert!(!output.trim().is_empty(), "Output is empty!");
+
+        Ok(())
+    }
 }
