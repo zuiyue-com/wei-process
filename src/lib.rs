e47965d52924abc6d5c745cf47d6ded8193d4a82
HuangWei <market@zuiyue.com>
2023-08-24 21:43:58 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#[cfg(target_os = "windows")]
 use std::os::windows::process::CommandExt;
 
 pub fn run(cmd: &str, param: Vec<String>) -> Result<String, Box<dyn std::error::Error>> {
@@ -13,9 +14,16 @@ pub fn run(cmd: &str, param: Vec<String>) -> Result<String, Box<dyn std::error::
 pub fn command(cmd: &str, param: Vec<String>) -> Result<String, Box<dyn std::error::Error>> {
     //let output = std::process::Command::new("powershell")
     // .args(&["/C", "start", "-wait", &format!("\"{}\"", cmd), param])
+
+    #[cfg(target_os = "windows")]
     let output = std::process::Command::new(cmd)
     .args(param)
     .creation_flags(winapi::um::winbase::CREATE_NO_WINDOW)
+    .output()?;
+
+    #[cfg(not(target_os = "windows"))]
+    let output = std::process::Command::new(cmd)
+    .args(param)
     .output()?;
 
     // let (res, _, _) = encoding_rs::UTF_16LE.decode(&output.stdout);
