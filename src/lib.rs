8073b4a534abbd618613ee8cf3ee11f3cd556d89
HuangWei <market@zuiyue.com>
2023-09-09 11:16:34 +0800

    0.1.22

--- src/lib.rs
+++ src/lib.rs
@@ -56,6 +56,36 @@ pub fn command(cmd: &str, param: Vec<String>) -> Result<String, Box<dyn std::err
     }    
 }
 
+/// Run wei command, If the program does not exist/ Under the data/directory, search for the program's configuration file
+/// # Arguments
+/// * `cmd` - Command name
+/// * `param` - Command parameters
+pub fn run_async(cmd: &str, param: Vec<String>) -> Result<(), Box<dyn std::error::Error>> {
+    #[cfg(target_os = "windows")]
+    let path = "./".to_owned() + cmd + ".exe";
+    #[cfg(not(target_os = "windows"))]
+    let path = "./".to_owned() + cmd;
+
+    info!("path: {:?}", path);
+
+    if let Ok(()) = command_async(&path, param.clone()) {
+        return Ok(());
+    };
+
+    #[cfg(target_os = "windows")]
+    let path = "./data/".to_owned() + cmd + ".exe";
+    #[cfg(not(target_os = "windows"))]
+    let path = "./data/".to_owned() + cmd;
+
+    if let Ok(()) = command_async(&path, param.clone()) {
+        return Ok(());
+    };
+
+    info!("{} dir: {:?}", cmd, wei_env::dir_bin());
+    let path = wei_env::read(&wei_env::dir_bin(),cmd)?;
+    command_async(path.as_str(), param)
+}
+
 pub fn command_async(cmd: &str, param: Vec<String>) -> Result<(), Box<dyn std::error::Error>> {
     #[cfg(target_os = "windows")]
     std::process::Command::new(cmd)
